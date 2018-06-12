55be0a3dc2d160f8d970c7eaa9b1cba9eb8d5c95
DESKTOP-4AM7V4O\Maayan <d>
2018-06-12 23:44:24 +0300

    Gave the rocket ship the abilty to fly

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidbody;
+	
+    // Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)) // can thrust while rotaiting
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
