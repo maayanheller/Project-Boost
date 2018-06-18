102fe693eb4aadf6a40f3e99fc3821a69bc1dd73
DESKTOP-4AM7V4O\Maayan <d>
2018-06-18 19:47:08 +0300

    Serialized fields Tagged gameobjects as friendly Created first level minor addons

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
     Rigidbody rigidbody;
     AudioSource audio;
@@ -20,16 +22,49 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
         Rotate();
+        Thrust();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("fuel");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidbody.freezeRotation = false; // resume physics control of rotation
+
     }
 
     private void Thrust()
     {
-        if (Input.GetKey(KeyCode.Space)) // can thrust while rotaiting
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) // can thrust while rotaiting
         {
             print("audio play");
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audio.isPlaying)
             {
                 audio.Play();
@@ -41,22 +76,5 @@ public class Rocket : MonoBehaviour
             audio.Stop();
             print("audio stop");
         }
-    }
-
-    private void Rotate()
-    {
-        rigidbody.freezeRotation = true;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-        }
-
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-        }
-
-        rigidbody.freezeRotation = false;
     }
 }
